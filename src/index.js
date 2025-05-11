30b13afa459c0a7e6afbdb8ecf6a93a2583dc330
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-11 16:39:10 +0600

    new commite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import job from "./lib/corn.js";
+import job from "./lib/cron.js"; // ✅ matches actual file
 
 import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", eventRoutes);
+app.use("/api/events", eventRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
