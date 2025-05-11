54bb421261ba4b42106499d1e4e6045779ad9d8c
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-11 16:08:23 +0600

    cron.js added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import "dotenv/config";
+import job from "./lib/corn.js";
 
-
-
-
-
-import authRoutes from "./routes/authRoutes.js"
-import eventRoutes from "./routes/eventRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
 
 import { connectDB } from "./lib/db.js";
 
-
 const app = express();
-const PORT=process.env.PORT||3000
+const PORT = process.env.PORT || 3000;
 
-
+job.start();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", eventRoutes);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/auth",eventRoutes)
-
-
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
+});
