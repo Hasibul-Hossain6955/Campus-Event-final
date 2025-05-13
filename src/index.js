816f8ad2f2c5998189d8aee3154523831cdbe4b9
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-13 21:34:44 +0600

    cron.js  added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-//import job from "./lib/cron.js"; // ✅ matches actual file
 
 import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//job.start();
+job.start();
 app.use(express.json());
 app.use(cors());
 
