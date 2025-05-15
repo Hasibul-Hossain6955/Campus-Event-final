6e58af1d6307566c51033aa33c991b2b74099136
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-15 21:40:09 +0600

    upadte login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,105 +1,108 @@
-import express from 'express';
-import User from '../models/User.js';
-import jwt from "jsonwebtoken"
-
+import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-const generateToken = (userId)=>{
- return jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"15d"});
-}
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
 
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
 
-router.post("/register",async(req,res)=>{
-    try {
-        const {email,username,password}=req.body
-
-        if (!email || !username || !password) {
-            return res.status(400).json({message: "All field are required"});
-            
-        }
-
-        if (password.length < 6) {
-            return res.status(400).json({message:"Password at least 6 character long"})
-        }
-        if(username.length < 3){
-            return res.status(400).json({message:"userName at least 3 character long"})
-        }
-
-        //check if user already exist
-        const existingEmail =  await User.findOne({email});
-        if (existingEmail) {
-           return res.status(400).json({message:"Email already exists"})
-        }
-
-        const existingUsername =  await User.findOne({username});
-        if (existingUsername) {
-           return res.status(400).json({message:"Username already exists"})
-        }
-
-            //get random avatar
-            const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`
-        const user = new User({
-            email,
-            username,
-            password,
-            profileImage
-        })
-
-       await user.save();
-
-
-       const token = generateToken(user._id)
-
-       res.status(201).json({
-        token,
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email,
-            profileImage:user.profileImage
-        },
-       })
-
-    } catch (error) {
-        console.log("Error in register route ",error)
-        res.status(500).json({message:"Internal server error"});
+    if (!email || !username || !password) {
+      return res.status(400).json({ message: "All field are required" });
     }
-})
- 
-//log in checking 
-router.post("/login",async(req,res)=>{
-    try {
-        const {email,password}= req.body;
-        if (!email || !password) {
-            return res.status(400).json({message: "All field are required"});
-        }
 
-        //check if user exists
-        const user = await User.findOne({email});
-        if (!user) {
-             return res.status(400).json({message: "Invalid Credential"});
-        }
-
-        //check if password is correct
-        const isPasswordCorrect = await user.comparePassword(password);
-        if (!isPasswordCorrect) {
-
-            return res.status(400).json({message: "Invalid Credential"});
-        }
-
-        //generate token
-        const token = generateToken(user._id);
-        res.status(200).json({
-            id:user._id,
-            username:user.username,
-            email:user.email,
-            profileImage:user.profileImage
-        })
-    } catch (error) {
-        console.log("Error in login route ",error)
-        res.status(500).json({message:"Internal server error"});
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password at least 6 character long" });
     }
-})
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "userName at least 3 character long" });
+    }
 
-export default router
+    //check if user already exist
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    //get random avatar
+    const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`;
+    const user = new User({
+      email,
+      username,
+      password,
+      profileImage,
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in register route ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//log in checking
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All field are required" });
+    }
+
+    //check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credential" });
+    }
+
+    //check if password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credential" });
+    }
+
+    //generate token
+    const token = generateToken(user._id);
+    res.status(200).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,153 +1,119 @@
-import  express from 'express';
+import express from "express";
 
 import cloudinary from "../lib/cloudinary.js";
-import Event from '../models/Event.js';
-import protectRoute from '../middleware/auth.middleware.js';
-
+import Event from "../models/Event.js";
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+//post
 
+router.post("/", protectRoute, async (req, res) => {
+  console.log("Incoming POST /events body:", req.body);
+  try {
+    const { title, caption, rating, image } = req.body;
 
-//post 
-
-router.post("/",protectRoute,async(req,res)=>{
-    try {
-        const {title, caption,rating,image} =req.body;
-
-        if (!title || !caption || !rating || image) {
-            return res.status(400).json({message:"Please provide all the fields"});
-            
-        }
-
-        //upload image to cloudinary
-       const uploadResponse= await cloudinary.uploader.upload(image)
-       const imageUrl = uploadResponse.secure_url
-
-        //save to the database
-        const newEvent = new Event({
-            title,
-            caption,
-            rating,
-            image:imageUrl,
-            user:req.user._id,
-
-        })
-
-        await newEvent.save();
-        res.status(201).json(newEvent);
-
-    } catch (error) {
-        console.log("Error Creating Event ",error)
-        res.status(500).json({message:error.message});
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ message: "Please provide all the fields" });
     }
-})
 
+    //upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
 
+    //save to the database
+    const newEvent = new Event({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
 
-
+    await newEvent.save();
+    res.status(201).json(newEvent);
+  } catch (error) {
+    console.log("Error Creating Event ", error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //pagination => infinite loading
-router.get("/", protectRoute, async(req,res)=>{
-    try {
-      
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    //example call from react native - front end
+    //const response = await fetch("http://localhost:3000/api/books?page=1&limit=5")
 
-        //example call from react native - front end
-        //const response = await fetch("http://localhost:3000/api/books?page=1&limit=5")
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
 
+    const events = await Event.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
 
-        const page = req.query.page ||1;
-        const limit = req.query.limit||5;
-        const skip = (page-1)*limit;
-
-        const events = await Event.find()
-        .sort({createdAt : -1})
-        .skip(skip)
-        .limit(limit)
-        .populate("user","username profileImage")
-         
-
-        const totalEvents = await Event.countDocuments();
-        res.send({
-            events,
-            currentPage:page,
-            totalEvents,
-            totalPages:Math.ceil(totalEvents/limit)
-        });
-        
-    } catch (error) {
-        console.log("Error in  get event route",error)
-        res.status(500).json({message:"Internal server Error"});
-    }
-})
-
-
-
+    const totalEvents = await Event.countDocuments();
+    res.send({
+      events,
+      currentPage: page,
+      totalEvents,
+      totalPages: Math.ceil(totalEvents / limit),
+    });
+  } catch (error) {
+    console.log("Error in  get event route", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+});
 
 //recommended event by the logged user
 
-router.get("/user",protectRoute,async(req,res)=>{
-    try {
-        const events = await Event.find({user:req.user._id}).sort({createdAt:-1});
-        res.json(events);
-    } catch (error) {
-        console.log("Get user event error",error.message)
-        res.status(500).json({message:"Internal server Error"});
-    }
-})
-
-
-
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const events = await Event.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.json(events);
+  } catch (error) {
+    console.log("Get user event error", error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+});
 
 //delete endpoint
 
-router.delete("/:id",protectRoute,async(req,res)=>{
-    try {
-         
-       const event =  await Event.findById(req.params.id);
-      
-      
-       if (!event) {
-        return res.status(404).json({message:"Event not found"});
-        
-       }
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
 
-       //check if user is the creator of this event
-         
-       if(event.user.toString()!= req.user._id.toString())
-       {
-        return  res.status(401).json({message:"Unauthorized"});
-       }
-
-
-     //delete image from cloudinary as well
-       if (event.image && event.image.includes("cloudinary")) {
-
-        try {
-            const publicId = event.image.split("/").pop().split(".")[0];
-            await cloudinary.uploader.destroy(publicId);
-
-            
-        } catch (deleteError) {
-            console.log("Error deleting image from cloudinary",deleteError)
-        }
-        
-       }    
-
-
-       await event.deleteOne();
-
-
-       res.json({message:"Event deleted successfully"})
-
-
-    } catch (error) {
-        console.log("Error deleting book",error)
-        res.status(500).json({message:"Internal server Error"});
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
     }
-})
 
+    //check if user is the creator of this event
 
+    if (event.user.toString() != req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
+    //delete image from cloudinary as well
+    if (event.image && event.image.includes("cloudinary")) {
+      try {
+        const publicId = event.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from cloudinary", deleteError);
+      }
+    }
+
+    await event.deleteOne();
+
+    res.json({ message: "Event deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting book", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+});
 
 export default router;
