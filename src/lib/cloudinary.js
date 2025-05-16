84b7de32a0673d9d838d0852a665585c06386af9
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-16 12:48:21 +0600

    new

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: "6--Ki71ZyEt1cSrYZ5h2aT_FX8k",
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 console.log("Cloudinary keys:", {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
