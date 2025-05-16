876ba9059dc75192c3197a727e00a5e8aef2413d
Hasibul-Hossain6955 <hasibulhasan6955@.com>
2025-05-16 16:04:43 +0600

    new commit

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -48,7 +48,7 @@ router.get("/", protectRoute, async (req, res) => {
     const limit = req.query.limit || 5;
     const skip = (page - 1) * limit;
 
-    const events = await Event.find()
+    const books = await Event.find()
       .sort({ createdAt: -1 })
       .skip(skip)
       .limit(limit)
